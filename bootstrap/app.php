05093849329872f468a1f341d08b3eefd02e5b21
quangnguyen11111111 <quangnguyen1507204@gmail.com>
2026-01-27 23:20:16 +0700

    submit middleware global

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Http\Middleware\CheckTimeAccess;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(CheckTimeAccess::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
